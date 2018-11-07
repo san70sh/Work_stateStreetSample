56c34e26702bd05fdb7f951ab5ceb6668b57dd76
Santosh <saisantosh.408@gmail.com>
2018-11-07 14:17:09 +0530

    Add files via upload

---
+++ sampleProject/src/test/java/sampleProject/JUnitTestSuite.java
@@ -0,0 +1,14 @@
+package sampleProject;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+	TestJunit1.class, TestJUnit2.class
+})
+
+public class JUnitTestSuite {
+	
+}
---
+++ sampleProject/src/test/java/sampleProject/TestJUnit2.java
@@ -0,0 +1,18 @@
+package sampleProject;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestJUnit2 {
+
+	String message = "Robert";
+	MessageUtil messageUtil = new MessageUtil(message);
+	
+	@Test
+	public void testSalutationMessage() {
+		System.out.println("Inside testSalutationMessage()");
+		message = "Hi!" + "Robert";
+		assertEquals(message,messageUtil.salutationMessage());
+	}
+}
