324d74723c136829c2fc78b7f48a80f1e8ba83c7
san70sh <saisantosh.408@gmail.com>
2018-11-10 15:41:10 +0530

    Updated and cleaned

--- sampleProject/src/main/java/com/sapient/service/PointOfSaleTerminal.java
+++ sampleProject/src/main/java/com/sapient/service/PointOfSaleTerminal.java
@@ -11,84 +11,84 @@ import java.util.Map;
 import com.sapient.models.Product;
 
 public class PointOfSaleTerminal {
-	
+
 	float totalPrice = 0.0f;
-	
-	Map<String, Product> orderList = new HashMap<String, Product>();
+	Product product;
+	Map<String, Product> productMap = new HashMap<String, Product>();
 
 	public void loadPrices() {
 		String fileRow = "";
-		File priceList = new File("../sampleProject/src/main/resources/pricing.txt");
-		
+		File priceDetails = new File("../sampleProject/src/main/resources/pricing.txt");
+
 		BufferedReader br = null;
 		try {
-			br = new BufferedReader(new FileReader(priceList));
-			
-			while((fileRow = br.readLine()) != null) {
-				List<String> productList = Arrays.asList(fileRow.split(";"));
-				Product product;
-				if(productList.size() == 5) {
-					product = new Product(productList.get(0),
-							Float.parseFloat(productList.get(1)),
-							productList.get(2),
-							Float.parseFloat(productList.get(3)),
-							Integer.parseInt(productList.get(4)));
-				} else {
-					product = new Product(productList.get(0),
-							Float.parseFloat(productList.get(1)),
-							"NA",
-							Float.parseFloat(productList.get(1)),
-							1);
+			br = new BufferedReader(new FileReader(priceDetails));
+
+			while ((fileRow = br.readLine()) != null) {
+				List<String> detailList = Arrays.asList(fileRow.split(";"));
+
+				for (int i = 0; i < detailList.size(); i++) {
+					List<String> productdetails = Arrays.asList(detailList.get(i).split(","));
+
+					if (productdetails.size() == 5) {
+						product = new Product(productdetails.get(0), Float.parseFloat(productdetails.get(1)),
+								productdetails.get(2), Float.parseFloat(productdetails.get(3)),
+								Integer.parseInt(productdetails.get(4)));
+					} else {
+						product = new Product(productdetails.get(0), Float.parseFloat(productdetails.get(1)), "NA",
+								Float.parseFloat(productdetails.get(1)), 1);
+					}
+					productMap.put(productdetails.get(0), product);
 				}
-				orderList.put(productList.get(0), product);
 			}
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			try {
-				if(br != null) {
+				if (br != null) {
 					br.close();
 				}
-			} catch(Exception e) {
-				
+			} catch (Exception e) {
+
 			}
 		}
-		
+
 	}
 
 	public void scan(String nextProduct) {
-		if (orderList.get(nextProduct) == null) {
+		if (productMap.get(nextProduct) == null) {
 			System.out.println("Product does not exist.");
 		} else {
-			totalPrice += this.updatePrice(orderList, nextProduct);
+			totalPrice += this.updatePrice(productMap, nextProduct);
 			System.out.println("Item added.");
 			System.out.printf("Current bill is $%.2f", totalPrice);
 			System.out.println();
 		}
-		
+
 	}
 
-	private float updatePrice(Map<String, Product> orderList, String nextProduct) {
-		
-		
-		if(orderList.get(nextProduct).getQty() - orderList.get(nextProduct).getCount() > 1) {
-			orderList.get(nextProduct).setCount(orderList.get(nextProduct).getCount()+1);
-			return orderList.get(nextProduct).getUnitPrice();
+	private float updatePrice(Map<String, Product> productMap, String nextProduct) {
+
+		Product currProduct = productMap.get(nextProduct);
+		if (currProduct.getQty()-currProduct.getCount()> 1) {
+			currProduct.setCount(currProduct.getCount() + 1);
+			return currProduct.getUnitPrice();
 		} else {
-			if (orderList.get(nextProduct).getVolume().equals("V")){
-				float corr = orderList.get(nextProduct).getVolPrice() - orderList.get(nextProduct).getCount() * orderList.get(nextProduct).getUnitPrice();
-				orderList.get(nextProduct).setCount(0);
+			if (currProduct.getVolume().equals("V")) {
+				float corr = currProduct.getVolPrice()
+						- currProduct.getCount() * currProduct.getUnitPrice();
+				currProduct.setCount(0);
 				return corr;
 			} else {
-				if(orderList.get(nextProduct).getQty() - orderList.get(nextProduct).getCount() == 1){
-					float corr = orderList.get(nextProduct).getQty() * orderList.get(nextProduct).getUnitPrice() - orderList.get(nextProduct).getCount() * orderList.get(nextProduct).getUnitPrice();
-					orderList.get(nextProduct).setCount(orderList.get(nextProduct).getCount()+1);
+				if (currProduct.getQty() - currProduct.getCount() == 1) {
+					float corr = currProduct.getQty() * currProduct.getUnitPrice()
+							- currProduct.getCount() * currProduct.getUnitPrice();
+					currProduct.setCount(currProduct.getCount() + 1);
 					return corr;
-			}
-				else {
-					return orderList.get(nextProduct).getVolPrice();
+				} else {
+					return currProduct.getVolPrice();
 				}
-				
+
 			}
 		}
 	}
